d16f15a1e38e4cc0311e7203d5555b132e96e0b3
Isaac Chukwuemeke Oselukwue <princeizak@live.com>
2021-05-27 20:45:23 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ExtensionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string demo = "This is a demo";
+            demo.PrintToConsole();
+        }
+    }
+    public static class Extensions
+    {
+        public static void PrintToConsole(this string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
